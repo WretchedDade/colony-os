3568e106c651108c345792681fd054b4eb62527d
Dade Cook <dadecook@gmail.com>
2026-09-14 01:05:40 -0400

    feat: bootstrap starter worker economy

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,33 @@ export const loop = ErrorMapper.wrapLoop((): void => {
     }
 
     cleanupDeadCreepMemory();
+
+    const spawn = Object.values(Game.spawns)[0];
+
+    if (!spawn) {
+        logger.warning("No spawn found. Please create a spawn to start the colony.");
+        return;
+    }
+
+    if (Object.keys(Game.creeps).length === 0) {
+        spawn.spawnCreep([WORK, CARRY, MOVE], "Worker");
+    }
+
+    for (const creep of Object.values(Game.creeps)) {
+        if (creep.store.getFreeCapacity() > 0) {
+            const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+        } else {
+            const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: structure =>
+                    structure.structureType === STRUCTURE_SPAWN && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            });
+
+            if (target && creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        }
+    }
 });
